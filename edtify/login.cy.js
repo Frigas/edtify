459ef66b62fbc1b78132d58abd7def9974bc2e48
Frigas <91873653+Frigas@users.noreply.github.com>
2024-02-25 16:23:47 +0100

    Update login.cy.js

--- edtify/login.cy.js
+++ edtify/login.cy.js
@@ -10,7 +10,9 @@ describe('Login Validation',()=>{
         cy.get(':nth-child(1) > .MuiInputBase-root > .MuiInputBase-input').type('archibonggodwin965@gmail.com')
         cy.get(':nth-child(2) > .MuiInputBase-root > .MuiInputBase-input').type('Godwin50058')
         cy.get('.MuiBox-root > .MuiButtonBase-root').click()
-        cy.screenshot();
+             cy.wait(6000)
+        cy.get('.css-76x2f2 > .MuiDrawer-root > .MuiPaper-root > [style="padding-top: 20px; background: rgb(252, 252, 252);"] > .MuiList-root > .css-1k0t4ux > .MuiButtonBase-root > .MuiListItemText-root > .MuiTypography-root').click()
+      
       
    
     })
